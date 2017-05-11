7237d0b1262f321f3d38edf7805b8a934e5de1b3
huangchangqiang <huangchangqiang@le.com>
2017-05-11 20:32:56 +0800

    commit

---
+++ src/pages/china/china.js
@@ -0,0 +1,10 @@
+
+var pageObject = {
+  data: {
+  },
+  chinaMapOnClick: function (event) {
+    console.log("123")
+  }
+}
+
+Page(pageObject)
---
+++ src/pages/world/world.js
@@ -0,0 +1,10 @@
+//index.js
+var pageObject = {
+  data: {
+  },
+  chinaMapOnClick: function (event) {
+    console.log("123")
+  }
+}
+
+Page(pageObject)
